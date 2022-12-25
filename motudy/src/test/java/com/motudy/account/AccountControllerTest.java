2b66723d5bb38809a492841b3d7dbc5d9217993c
Bruce Han <intrager@naver.com>
2022-12-25 15:48:39 +0900

    Member registration navigation menu & Frontend library setup

---
+++ motudy/src/test/java/com/motudy/account/AccountControllerTest.java
@@ -0,0 +1,120 @@
+package com.motudy.account;
+
+import com.motudy.domain.Account;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc // @AutoConfigureWebTestClient를 하면 테스트 하면서 view 응답을 다 보여줌. 즉, view test가 가능
+class AccountControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Autowired private AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
+
+    @DisplayName("인증 메일 확인 - 입력값 오류")
+    @Test
+    void checkEmailToken_with_wrong_input() throws Exception {
+        mockMvc.perform(get("/check-email-token")
+                .param("token", "helloworld")
+                .param("email", "motudy@email.com"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("error"))
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(unauthenticated()); // Spring Security를 쓰면 mockMvc에 사용 가능
+    }
+
+    @DisplayName("인증 메일 확인 - 입력값 정상")
+    @Test
+    void checkEmailToken() throws Exception {
+        Account account = Account.builder()
+                .email("motudy@email.com")
+                .password("12345678")
+                .nickname("motudy")
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
+
+        mockMvc.perform(get("/check-email-token")
+                .param("token", newAccount.getEmailCheckToken())
+                .param("email", newAccount.getEmail()))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("error"))
+                .andExpect(model().attributeExists("nickname"))
+                .andExpect(model().attributeExists("numberOfUser"))
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(authenticated().withUsername("motudy")); // "인증이 된"
+    }
+
+    /**
+     * 시큐리티가 적용되면
+     * sign-up 폼에 접근할 때 access denied exception이 발생하고
+     * 이걸 시큐리티 필터가 잡아서 로그인 폼 생성과 함께 폼을 보여주도록 함
+     * view가 제공된다고 볼 수 있음
+     */
+    @DisplayName("회원 가입 화면 보이는지 테스트")
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk()) // 200 응답 확인
+                .andExpect(view().name("account/sign-up")) // 뷰
+                .andExpect(model().attributeExists("signUpForm")) // 모델 객체
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 오류")
+    @Test
+    void signUpSubmit_with_wrong_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("nickname", "motudy")
+                .param("email", "email..")
+                .param("password", "12345")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 정상")
+    @Test
+    void signUpSubmit_with_correct_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("nickname", "motudy")
+                .param("email", "motudy@email.com")
+                .param("password", "12345678")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated().withUsername("motudy"));
+
+        Account account = accountRepository.findByEmail("motudy@email.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "12345678");
+        assertNotNull(account.getEmailCheckToken());
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
+    }
+}
