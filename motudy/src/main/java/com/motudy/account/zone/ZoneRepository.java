a4796021631d4d8d42cee28bf8cdda61923137e5
Bruce Han <intrager@naver.com>
2022-12-31 21:49:39 +0900

    zone domain

---
+++ motudy/src/main/java/com/motudy/account/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.motudy.account.zone;
+
+import com.motudy.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
