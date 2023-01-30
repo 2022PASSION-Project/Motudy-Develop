17756cf8856a59eeb7d02529227eb0c0bbd3b2b3
Bruce Han <intrager@naver.com>
2023-01-31 00:15:52 +0900

    Change notification icon

--- motudy/src/main/java/com/motudy/infra/config/WebConfig.java
+++ motudy/src/main/java/com/motudy/infra/config/WebConfig.java
@@ -2,9 +2,14 @@ package com.motudy.infra.config;
 
 import com.motudy.modules.notification.NotificationInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Web MVC를 커스터마이징 하고 싶을 때
@@ -19,8 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        List<String> staticResourcesPath = Arrays.stream(StaticResourceLocation.values())
+                        .flatMap(StaticResourceLocation::getPatterns)
+                        .collect(Collectors.toList());
+        staticResourcesPath.add("/node_modules/**");
 
         registry.addInterceptor(notificationInterceptor) // 여기까지만 하면 모든 요청에 인터셉터가 적용됨
-                .excludePathPatterns();
+                .excludePathPatterns(staticResourcesPath);
     }
 }
